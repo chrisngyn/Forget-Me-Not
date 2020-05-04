594c975b016375783d56a8cefc4e6bbdc83b5eba
Christopher Nguyen <chrisngyn99@gmail.com>
2020-05-04 03:34:32 -0400

    completed filtering

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -18,7 +18,8 @@ Todo List app requirements - courtesy of https://www.youtube.com/watch?v=I6IY2Tq
 export default class TodoList extends React.Component {
     state = {
         // we'll store them as an array that we keep updating over time
-        entries: []
+        entries: [],
+        currentState: "all" // by default we'll show all, as the user clicks on buttons we'll change this
     }
 
     addEntry = (obj) => {
@@ -48,13 +49,56 @@ export default class TodoList extends React.Component {
         })
     }
 
+    toggleAll = () => {
+        this.setState({
+            currentState: "all"
+        })
+    }
+
+    toggleActive = () => {
+        this.setState({
+            currentState: "active"
+        })
+    }
+
+    toggleComplete = () => {
+        this.setState({
+            currentState: "completed"
+        })
+    }
+
     render() {
+        let display = [];
+
+        if (this.state.currentState === "all") {
+            display = this.state.entries;
+        } else if (this.state.currentState === "active") {
+            display = this.state.entries.filter(element => element.complete === false)
+        } else {
+            display = this.state.entries.filter(element => element.complete)
+        }
+
         return(
-            // adding onSubmit as a prop
             <div>
+                <div>
+                    Active: { // go through one by one and see if the element is incomplete
+                        this.state.entries.filter(element => element.complete === false).length
+                    }
+                </div>
+                
+
                 <TodoForm onSubmit={this.addEntry}/>
+
+
+                <div>
+                    <button onClick={this.toggleAll}>Show ALL</button>
+                    <button onClick={this.toggleActive}>Show ACTIVE</button>
+                    <button onClick={this.toggleComplete}>Show COMPLETED</button>
+                </div>
+
+
                 {
-                    this.state.entries.map(element => ( // each element will map to a "Entry" to display with an ID
+                    display.map(element => ( // each element will map to a "Entry" to display with an ID
                         <Entry
                             key={element.id}
                             completeEntry={() => this.completeEntry(element.id)} // pass a function as one of the prop fields so we can update
